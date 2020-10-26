a2c9b6fde6c99cefe652010f9e011968dc28a021
sdpoulose <sdpoulose@gmail.com>
2020-10-26 12:54:55 -0400

    bingoTitle updates on click, added submit button

--- src/components/bingoItemList.jsx
+++ src/components/bingoItemList.jsx
@@ -9,14 +9,48 @@ class BingoItemList extends Component {
     render () {
         return ( 
             <div> 
+                <form> 
+                    <input 
+                        type = "text" 
+                        name = "title" 
+                        id = "title"
+                    /> 
+                    <input 
+                        className = "btn btn-danger m-2 btn-block" 
+                        type = "reset" 
+                        value = "Reset" 
+                        name = "Reset" 
+                    /> 
+                </form>
                 <BingoItem/>
                 <BingoItem/>
                 <BingoItem/>
                 <BingoItem/>
                 <BingoItem/>
-                <input className = "btn-success" type = "submit" value = "Submit" />
+                <input 
+                    className = "btn-primary btn-lg" 
+                    type = "submit" 
+                    value = "Done" 
+                    onClick = {this.handleDone}
+                />
+                <input 
+                    className = "btn-success btn-block btn-lg m-2" 
+                    type = "submit" 
+                    value = "Submit" 
+                    onClick = {this.handleSubmit}
+                />
             </div>
         );
+    }
+
+    handleDone = () => {
+        var bingoTitle = document.getElementById("title").value;
+        this.setState({bingoTitle: bingoTitle});
+        //console.log(this.state.bingoTitle);
+    }
+
+    handleSubmit = () => {
+        console.log("");
     }
 }
 
