3de2923b8795caea0adc276e1c562250b78ec60f
sdpoulose <sdpoulose@gmail.com>
2020-11-19 20:19:53 -0500

    deleted old files to start over

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import BingoItemList from './components/bingoItemList';
-
-
-ReactDOM.render(<BingoItemList />, document.getElementById("root"));
