cfeb99b0ac9cb3dedbca18ad8c8634776d4b00b9
sdpoulose <sdpoulose@gmail.com>
2021-01-24 18:08:04 -0500

    removed test component import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import TestComponent from './Components/testComponent'
 import BingoBox from './Components/bingoBox'
 
 ReactDOM.render(<BingoBox />, document.getElementById('root'))
