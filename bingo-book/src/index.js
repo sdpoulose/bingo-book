553146c44d3ac78f754fd293f2fb4a2d6d4b7bf6
sdpoulose <sdpoulose@gmail.com>
2021-01-24 14:56:28 -0500

    removed react app to start over with electron wrapper

--- bingo-book/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import BingoBox from "./components/bingoBox";
-
-ReactDOM.render(<BingoBox />, document.getElementById("root"));
