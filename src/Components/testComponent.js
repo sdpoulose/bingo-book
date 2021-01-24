589ff10b484f1e3951f6b06441cdeb03df79d282
sdpoulose <sdpoulose@gmail.com>
2021-01-24 16:01:13 -0500

    tested working react components in electron desktop app

---
+++ src/Components/testComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default function TestComponent() {
+
+    return (
+        <h1> React component works </h1 >
+    )
+
+}
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello React World</h1>, document.getElementById('root'))
+import TestComponent from './Components/testComponent'
+
+ReactDOM.render(<TestComponent />, document.getElementById('root'))
