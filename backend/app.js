ca19759805bca8d2243d2eee84c9267d9f8b0734
sdpoulose <sdpoulose@gmail.com>
2021-02-02 19:52:41 -0500

    added starter code for connection to mongodb atlas, json parser, and env variables

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = express();
+
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
 //ROUTES
 app.get('/', (req, res) => {
