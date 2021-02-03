8298b0d24eac9e434b49ece030bfbdc0166b1ab0
sdpoulose <sdpoulose@gmail.com>
2021-02-03 14:47:35 -0500

    added some endpoints to router

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,3 +1,10 @@
 const router = require('express').Router();
 
 let Card = require('../models/card.model');
+
+router.route('/').get((req, res) => {
+    Card.find()
+        .then(cards => res.json(cards))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
