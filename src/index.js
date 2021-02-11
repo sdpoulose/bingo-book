b9df490b28c7f576864665a2783f7d676f039a14
sdpoulose <sdpoulose@gmail.com>
2021-02-10 21:57:04 -0500

    added some styling

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 
 import BingoBox from './Components/bingoBox'
 
+import './index.scss';
+
 ReactDOM.render(<BingoBox />, document.getElementById('root'))
 //ReactDOM.render(<BingoBox />, document.getElementById('root'))
