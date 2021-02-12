d1ec2bcf73ebc5d2b92649dad9570b5df0d4cf50
sdpoulose <sdpoulose@gmail.com>
2021-02-11 20:06:38 -0500

    server working again

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-/* const express = require('express');
+const express = require('express');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
@@ -19,10 +19,4 @@ app.get('/', (req, res) => {
     res.send("Hello World!")
 });
 
-const cardsRouter = require('./routes/cards');
-
-app.use('./cards', cardsRouter);
-
 app.listen(3000, () => console.log("Server started on Port 3000"));
-
-*/
