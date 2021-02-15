8075114f32ab6b334eedc6abfc466f0bef715556
sdpoulose <sdpoulose@gmail.com>
2021-02-15 12:28:33 -0500

    added card schema and set trim to true

--- backend/models/card.model.js
+++ backend/models/card.model.js
@@ -1,10 +1,11 @@
-/* const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
 const cardSchema = new Schema({
     title: {
-        type: String
+        type: String,
+        trim: true
     },
     content: {
         type: [String]
@@ -14,5 +15,3 @@ const cardSchema = new Schema({
 const Card = mongoose.model('Card', cardSchema);
 
 module.exports = Card;
-
-*/
